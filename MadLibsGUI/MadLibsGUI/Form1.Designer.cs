f6c418e66bcb6f68420c8cb3f7820108cf96447b
kelleywohlers15 <57108291+kelleywohlers15@users.noreply.github.com>
2019-11-21 15:11:33 -0600

    updated madlibs

---
+++ MadLibsGUI/MadLibsGUI/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace MadLibsGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtcolor = new System.Windows.Forms.TextBox();
+            this.txtnoun = new System.Windows.Forms.TextBox();
+            this.txtwordest = new System.Windows.Forms.TextBox();
+            this.txtpluralnoun = new System.Windows.Forms.TextBox();
+            this.txtbodypartsplural = new System.Windows.Forms.TextBox();
+            this.txtpicknumber1 = new System.Windows.Forms.TextBox();
+            this.txtanimal = new System.Windows.Forms.TextBox();
+            this.txtpicknumber2 = new System.Windows.Forms.TextBox();
+            this.btncreate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblresult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtcolor
+            // 
+            this.txtcolor.Location = new System.Drawing.Point(108, 63);
+            this.txtcolor.Name = "txtcolor";
+            this.txtcolor.Size = new System.Drawing.Size(100, 20);
+            this.txtcolor.TabIndex = 0;
+            // 
+            // txtnoun
+            // 
+            this.txtnoun.Location = new System.Drawing.Point(303, 63);
+            this.txtnoun.Name = "txtnoun";
+            this.txtnoun.Size = new System.Drawing.Size(100, 20);
+            this.txtnoun.TabIndex = 1;
+            // 
+            // txtwordest
+            // 
+            this.txtwordest.Location = new System.Drawing.Point(108, 109);
+            this.txtwordest.Name = "txtwordest";
+            this.txtwordest.Size = new System.Drawing.Size(100, 20);
+            this.txtwordest.TabIndex = 2;
+            // 
+            // txtpluralnoun
+            // 
+            this.txtpluralnoun.Location = new System.Drawing.Point(303, 109);
+            this.txtpluralnoun.Name = "txtpluralnoun";
+            this.txtpluralnoun.Size = new System.Drawing.Size(100, 20);
+            this.txtpluralnoun.TabIndex = 3;
+            // 
+            // txtbodypartsplural
+            // 
+            this.txtbodypartsplural.Location = new System.Drawing.Point(108, 155);
+            this.txtbodypartsplural.Name = "txtbodypartsplural";
+            this.txtbodypartsplural.Size = new System.Drawing.Size(100, 20);
+            this.txtbodypartsplural.TabIndex = 4;
+            // 
+            // txtpicknumber1
+            // 
+            this.txtpicknumber1.Location = new System.Drawing.Point(303, 155);
+            this.txtpicknumber1.Name = "txtpicknumber1";
+            this.txtpicknumber1.Size = new System.Drawing.Size(100, 20);
+            this.txtpicknumber1.TabIndex = 5;
+            // 
+            // txtanimal
+            // 
+            this.txtanimal.Location = new System.Drawing.Point(108, 210);
+            this.txtanimal.Name = "txtanimal";
+            this.txtanimal.Size = new System.Drawing.Size(100, 20);
+            this.txtanimal.TabIndex = 6;
+            // 
+            // txtpicknumber2
+            // 
+            this.txtpicknumber2.Location = new System.Drawing.Point(303, 210);
+            this.txtpicknumber2.Name = "txtpicknumber2";
+            this.txtpicknumber2.Size = new System.Drawing.Size(100, 20);
+            this.txtpicknumber2.TabIndex = 7;
+            // 
+            // btncreate
+            // 
+            this.btncreate.Location = new System.Drawing.Point(157, 275);
+            this.btncreate.Name = "btncreate";
+            this.btncreate.Size = new System.Drawing.Size(126, 26);
+            this.btncreate.TabIndex = 8;
+            this.btncreate.Text = "click to create";
+            this.btncreate.UseVisualStyleBackColor = true;
+            this.btncreate.Click += new System.EventHandler(this.btncreate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "color";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "word ending in est";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "body part plural";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "animal ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(266, 56);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "noun";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(238, 102);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "plural noun";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(223, 161);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "pick a number";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(220, 213);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(77, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "pick a number ";
+            // 
+            // lblresult
+            // 
+            this.lblresult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblresult.Location = new System.Drawing.Point(12, 323);
+            this.lblresult.Name = "lblresult";
+            this.lblresult.Size = new System.Drawing.Size(401, 56);
+            this.lblresult.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 450);
+            this.Controls.Add(this.lblresult);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btncreate);
+            this.Controls.Add(this.txtpicknumber2);
+            this.Controls.Add(this.txtanimal);
+            this.Controls.Add(this.txtpicknumber1);
+            this.Controls.Add(this.txtbodypartsplural);
+            this.Controls.Add(this.txtpluralnoun);
+            this.Controls.Add(this.txtwordest);
+            this.Controls.Add(this.txtnoun);
+            this.Controls.Add(this.txtcolor);
+            this.Name = "Form1";
+            this.Text = "mad Libs";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtcolor;
+        private System.Windows.Forms.TextBox txtnoun;
+        private System.Windows.Forms.TextBox txtwordest;
+        private System.Windows.Forms.TextBox txtpluralnoun;
+        private System.Windows.Forms.TextBox txtbodypartsplural;
+        private System.Windows.Forms.TextBox txtpicknumber1;
+        private System.Windows.Forms.TextBox txtanimal;
+        private System.Windows.Forms.TextBox txtpicknumber2;
+        private System.Windows.Forms.Button btncreate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblresult;
+    }
+}
+
---
+++ MadLibsGUI/MadLibsGUI/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MadLibsGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncreate_Click(object sender, EventArgs e)
+        {
+            double num;
+            num = Convert.ToDouble(txtpicknumber1.Text) - Convert.ToDouble(txtpicknumber2.Text);
+
+            lblresult.Text = "The " + txtcolor.Text + " Dragon is the " + txtwordest.Text +
+                " Dragon of all. It has " + num + " " + txtbodypartsplural.Text +
+                ", and a " + txtanimal.Text + " shaped like a " + txtbodypartsplural.Text +
+                ", although it well feast on nearly anything.";
+
+
+        }
+   }
+}
